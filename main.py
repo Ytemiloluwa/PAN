f8ed84d6aa1ceeb296bfd91caaedcd8813d39e81
ytemiloluwa <ytemiloluwa@gmail.com>
2024-05-20 19:48:28 +0100

    main file

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk, scrolledtext, Menu
+from tkinter.font import Font
+from ui import setup_ui
+
+# Create instance
+win = tk.Tk()
+win.title("PAN GENERATOR")
+win.iconbitmap("/Users/temi/Downloads")
+
+# Setup UI components
+
+
+# Start the GUI
+win.mainloop()
