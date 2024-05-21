87be72e79c067e456828c05d66db7a2dc8521837
ytemiloluwa <ytemiloluwa@gmail.com>
2024-05-21 12:47:43 +0100

    pan: validate expiry date

--- validator.py
+++ validator.py
@@ -1,4 +1,5 @@
 from utils import luhn_check, bin_ranges
+from datetime import datetime
 
 def validate_pan(pan):
     if not luhn_check(pan):
@@ -11,7 +12,16 @@ def detect_card_brand(pan):
     brands = {
         '4': 'Visa',
         '5': 'MasterCard',
-        # Add more brands here
+        '3':  'American Express'
     }
     return brands.get(pan[0], "Unknown")
 
+def validate_expiry_date(expiry_date):
+    try:
+        exp_date = datetime.strptime(expiry_date, "%m/%y")
+        if exp_date > datetime.now():
+            return True, "Card is not expired"
+        else:
+            return False, "Card is expired"
+    except ValueError:
+        return False, "Invalid date format"
