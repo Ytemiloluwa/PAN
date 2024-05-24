b8495b16a8519570a24739cc65dcaa2e5bbd5b77
ytemiloluwa <ytemiloluwa@gmail.com>
2024-05-24 06:21:07 +0100

    pan: generator

--- generator.py
+++ generator.py
@@ -2,10 +2,18 @@ import numpy as np
 import random
 from utils import luhn_check, bin_ranges
 
-def generate_pan(bin_input, count=1):
+CARD_BRANDS = {
+    'Visa': '4',
+    'Mastercard': '5',
+    'American Express': '3'
+}
+
+def generate_pan(bin_input, count=1, brand=None):
     pan_list = []
+    prefix = CARD_BRANDS.get(brand, '') if brand else ''
     for _ in range(count):
         temp_pan = ''.join([str(random.randint(0, 9)) if c == '?' else c for c in bin_input])
+        temp_pan = prefix + temp_pan[len(prefix):]
         if luhn_check(temp_pan) and bin_ranges(temp_pan):
             pan_list.append(temp_pan)
     return pan_list
