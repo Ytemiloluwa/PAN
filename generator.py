b2a94cc027f2b2c65af808fab0ef0bc8e1064794
ytemiloluwa <ytemiloluwa@gmail.com>
2024-05-24 17:52:53 +0100

    pan: generate

--- generator.py
+++ generator.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
-from utils import luhn_check, bin_ranges
+import re
+from utils import luhn_check, bin_ranges, IIN_Ranges, IIN_Ranges_List
+
 
 CARD_BRANDS = {
     'Visa': '4',
@@ -8,15 +10,37 @@ CARD_BRANDS = {
     'American Express': '3'
 }
 
-def generate_pan(bin_input, count=1, brand=None):
-    pan_list = []
-    prefix = CARD_BRANDS.get(brand, '') if brand else ''
-    for _ in range(count):
-        temp_pan = ''.join([str(random.randint(0, 9)) if c == '?' else c for c in bin_input])
-        temp_pan = prefix + temp_pan[len(prefix):]
-        if luhn_check(temp_pan) and bin_ranges(temp_pan):
-            pan_list.append(temp_pan)
-    return pan_list
+def generate_pan(credit_card):
+    if credit_card.count("?") > 0 and len(credit_card) <= 16:
+        possibilities = np.uint64(10 ** credit_card.count("?"))
+        counter = 0
+
+        IIN_tuple_ranges = IIN_Ranges_List()
+
+        PANasList = list(credit_card)  # Convert the PAN number into a List
+
+        L = []  # A list with the indexes of the ? characters as found in given PAN
+        for m in re.finditer('\?', credit_card):
+            L.append(m.start())
+
+        valid_pans = []
+        while counter < possibilities:
+            STR = str(counter).zfill(credit_card.count("?"))
+            counter += 1
+
+            for index, char in zip(L, STR):
+                PANasList[index] = char
+
+            tempPAN = "".join(PANasList)
+
+            if is_luhn_valid(tempPAN) and IIN_Ranges(tempPAN, IIN_tuple_ranges):
+                valid_pans.append(tempPAN)
+
+        return valid_pans
+
+# function to check if a card number is valid based on the luhn algorithm.
+def is_luhn_valid(card_number):
+    return luhn_check(card_number) == 0
 
 def generate_cvv():
     return str(random.randint(100, 999))
