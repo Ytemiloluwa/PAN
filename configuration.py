b18b1ad4ea8cd79d5d3ac3ab6d4854adeecfb476
Ytemiloluwa <ytemiloluwa@gmail.com>
2025-05-26 05:24:29 +0100

    chore: Add Readme

--- configuration.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-DEBUG_MODE = True
-
-def setup_logging():
-    if DEBUG_MODE:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.INFO)
